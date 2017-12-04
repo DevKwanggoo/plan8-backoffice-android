2d563efcf09c23c146fa75da79d73015dea2481a
김형규 <hyungkyu@starshell.co>
2017-12-04 21:16:32 +0900

    원시그널 붙여놓음

--- app/src/main/java/io/plan8/backoffice/BaseApplication.java
+++ app/src/main/java/io/plan8/backoffice/BaseApplication.java
@@ -3,6 +3,7 @@ package io.plan8.backoffice;
 import android.support.multidex.MultiDexApplication;
 
 import com.google.firebase.FirebaseApp;
+import com.onesignal.OneSignal;
 
 import io.intercom.android.sdk.Intercom;
 import io.plan8.backoffice.adapter.RestfulAdapter;
@@ -19,5 +20,9 @@ public class BaseApplication extends MultiDexApplication {
         FirebaseApp.initializeApp(getApplicationContext());
         Intercom.initialize(this, "android_sdk-5efb4215c714d457c400f5fe0297cd84e265c758", "pno474gz");
         RestfulAdapter.build(getApplicationContext());
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
     }
 }
