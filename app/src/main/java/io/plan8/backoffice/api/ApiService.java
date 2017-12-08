09426ca6c70b2a8a78f00ead45c6b219a7def1cd
김형규 <hyungkyu@starshell.co>
2017-12-08 15:52:27 +0900

    알림 API 선행작업

--- app/src/main/java/io/plan8/backoffice/api/ApiService.java
+++ app/src/main/java/io/plan8/backoffice/api/ApiService.java
@@ -10,6 +10,7 @@ import io.plan8.backoffice.model.api.Member;
 import io.plan8.backoffice.model.api.Reservation;
 import io.plan8.backoffice.model.api.Team;
 import io.plan8.backoffice.model.api.Upload;
+import io.plan8.backoffice.model.item.NotificationItem;
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -80,4 +81,7 @@ public interface ApiService {
 
     @GET("1/users/me/members")
     Call<List<Member>> getTeamMembers(@Header("authorization") String auth);
+
+    @GET("1/teams/{id}/notification")
+    Call<List<NotificationItem>> getNotifications(@Header("authorization") String auth, @Path("id") int teamId);
 }
--- app/src/main/java/io/plan8/backoffice/vm/item/NotificationItemVM.java
+++ app/src/main/java/io/plan8/backoffice/vm/item/NotificationItemVM.java
@@ -3,6 +3,8 @@ package io.plan8.backoffice.vm.item;
 import android.databinding.Bindable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.Toast;
 
 import io.plan8.backoffice.model.item.NotificationItem;
 import io.plan8.backoffice.vm.FragmentVM;
@@ -40,5 +42,9 @@ public class NotificationItemVM extends FragmentVM {
             return notificationItem.getLastModified();
         }
         return "";
+    }
+
+    public void detailNotification(View view){
+        Toast.makeText(getFragment().getContext(), "알림 자세히 보기", Toast.LENGTH_SHORT).show();
     }
 }
