267d68bec2115dcc4f1ec71365932f175906ab0a
김형규 <hyungkyu@starshell.co>
2017-12-13 20:13:59 +0900

    핸들러추가중

--- app/src/main/java/io/plan8/backoffice/vm/item/DetailReservationActionFileItemVM.java
+++ app/src/main/java/io/plan8/backoffice/vm/item/DetailReservationActionFileItemVM.java
@@ -5,11 +5,16 @@ import android.content.Intent;
 import android.databinding.Bindable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import io.plan8.backoffice.BR;
 import io.plan8.backoffice.R;
 import io.plan8.backoffice.activity.PreviewActivity;
+import io.plan8.backoffice.manager.RealTimeHandlerManager;
 import io.plan8.backoffice.model.api.Action;
 import io.plan8.backoffice.util.DateUtil;
 import io.plan8.backoffice.util.FileDownloader;
@@ -21,10 +26,25 @@ import io.plan8.backoffice.vm.ActivityVM;
 
 public class DetailReservationActionFileItemVM extends ActivityVM {
     private Action action;
+    private Handler handler;
 
-    public DetailReservationActionFileItemVM(Activity activity, Bundle savedInstanceState, Action action) {
+    public DetailReservationActionFileItemVM(Activity activity, Bundle savedInstanceState, final Action action) {
         super(activity, savedInstanceState);
         this.action = action;
+
+        handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                setCreated();
+                Log.e(action.getType() + "-updated : ", "OK");
+                this.sendEmptyMessageDelayed(0, 30000);
+            }
+        };
+        RealTimeHandlerManager.getInstance().addActionHandler(handler);
+        RealTimeHandlerManager.getInstance().addHandler("action", RealTimeHandlerManager.getInstance().getActionHandler());
+        handler.sendEmptyMessage(0);
     }
 
     @Bindable
@@ -92,5 +112,9 @@ public class DetailReservationActionFileItemVM extends ActivityVM {
         } else {
             Toast.makeText(getActivity(), "파일을 다운로드 할 수 없습니다.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void setCreated() {
+        notifyPropertyChanged(BR.createdDate);
     }
 }
